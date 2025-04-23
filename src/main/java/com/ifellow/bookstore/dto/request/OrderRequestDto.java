e8854f71b044fba7afe47e6281623717b7d69da1
Семён Мерченко <smerchenko@test.ru>
2025-04-24 00:39:03 +0300

    Рефакторинг контроллеров

--- src/main/java/com/ifellow/bookstore/dto/request/OrderRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.ifellow.bookstore.dto.request;
-
-import java.util.List;
-
-public record OrderRequestDto(
-        Long warehouseId,
-        List<BookOrderDto> bookOrderDtoList
-) {
-}
